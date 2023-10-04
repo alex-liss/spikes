14026f823e9fae4f42c115370685c0858a349a47
alex.karely <alex.karely@serviceware.de>
2023-10-04 08:23:44 +0200

    added basic chart with Apache ECharts

--- charts/src/app/pages/chartjs/chartjs.component.ts
+++ charts/src/app/pages/chartjs/chartjs.component.ts
@@ -11,7 +11,7 @@ export class ChartjsComponent {
   public chart: any;
 
   ngOnInit(): void {
-    this.chart = new Chart("chart", {
+    this.chart = new Chart("chartjs", {
       type: 'line',
       data: {// values on X-Axis
         labels: ['1','2','3','4','5','6','7','8', ],
--- charts/src/app/pages/echarts/echarts.component.ts
+++ charts/src/app/pages/echarts/echarts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-echarts',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class EchartsComponent {
 
+  ngOnInit() {
+    let chart = echarts.init(document.getElementById('echarts'));
+    chart.setOption({
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['1','2','3','4','5','6','7','8',]
+      },
+      yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%']
+      },
+      series: [
+        {
+          name:'one',
+          type:'line',
+          data: ['467','576', '572', '79', '92','574', '573', '576']
+        },
+        {
+          name:'two',
+          type:'line',
+          data: ['542', '542', '536', '327', '17','0.00', '538', '541']
+        }
+      ]
+    })
+  }
 }
